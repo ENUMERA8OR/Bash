6dd65fc20fefce19477b36374325bb6f49d80c7b
Aditya Tiwari <65213780+ENUMERA8OR@users.noreply.github.com>
2024-01-18 15:44:52 -0500

    Update IDS.sh

--- IDS.sh
+++ IDS.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/bash 
 # Calls all the other functions in the correct order
 main() {
     
@@ -10,33 +9,49 @@ main() {
 }
 
 # Reads the log file line by line
-read_log_file() {
+read_log_file(file_name) {
     # Implementation
+    while read file_name; do
+    echo "$file_name"
+    done < file_name
+    send_alert "$file_name"
+    handle_false_positives "$file_name"
+
 }
 
-# Parses the log file to extract relevant information
-parse_log_file() {
+upload_threat_signatures() {
     # Implementation
+    echo "Insert your threat signature file"
+    read -p "Threat signature file entered" sig_file
+    send_alert "$sig_file"
+    handle_false_positives "$sig_file"
 }
 
-# Analyzes the parsed log file to identify potential threats
-analyze_log_file() {
+# Handles false positives
+handle_false_positives() {
     # Implementation
-}
-
-# Checks the parsed log file against the threat signatures
-check_threat_signatures() {
-    # Implementation
-}
-
-# Handles false positives and negatives
-handle_false_positives_negatives() {
-    # Implementation
+    readarray -t array3 < $file_name
+    readarray -t array4 < $sig_file
 }
 
 # Alerts the system administrator
-send_alert() {
+send_alert(file_name,sig_file) {
     # Implementation
+    readarray -t array1 < $file_name
+    readarray -t array2 < $sig_file
+    for compare1 in "$array[[@]]";do
+        for compare2 in "$array2[[@]]";do
+            if [[ $compare1[] -eq $compare2[] ]];then
+                
+                echo "Alert detected!"
+
+            else
+
+                echo "No alert was detected in the log file."
+            fi
+        done
+    done
+
 }
 
 main

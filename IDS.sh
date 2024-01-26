2b0ef8d16100d62f0612c0925e2804b3763c6cb1
Aditya Tiwari <65213780+ENUMERA8OR@users.noreply.github.com>
2024-01-26 14:23:06 +0530

    Update IDS.sh

--- IDS.sh
+++ IDS.sh
@@ -4,50 +4,63 @@ main() {
     
     # Implementation
     echo "Insert your log file"
-    read -p "Log file entered" file_name
-    read_log_file "$file_name"
+    read -p "Log file entered" file_name_path
+    read_log_file "$file_name_path"
 }
 
 # Reads the log file line by line
 read_log_file(file_name) {
     # Implementation
-    while read file_name; do
-    echo "$file_name"
-    done < file_name
-    send_alert "$file_name"
-    handle_false_positives "$file_name"
+    while read file_name_path; do
+    echo "$file_name_path"
+    done < file_name_path
+    send_alert "$file_name_path"
+    handle_false_positives "$file_name_path"
 
 }
 
+# Upload your threat signature suricata rules file
 upload_threat_signatures() {
     # Implementation
     echo "Insert your threat signature file"
     read -p "Threat signature file entered" sig_file
-    send_alert "$sig_file"
-    handle_false_positives "$sig_file"
+    send_alert "$sig_file_path"
+    handle_false_positives "$sig_file_path"
 }
 
 # Handles false positives
 handle_false_positives() {
     # Implementation
-    readarray -t array3 < $file_name
-    readarray -t array4 < $sig_file
+    readarray -t array3 < $file_name_path
+    readarray -t array4 < $sig_file_path
 }
 
 # Alerts the system administrator
-send_alert(file_name,sig_file) {
+send_alert(file_name_path,sig_file_path) {
     # Implementation
-    readarray -t array1 < $file_name
-    readarray -t array2 < $sig_file
+    readarray -t array1 < $file_name_path
+    readarray -t array2 < $sig_file_path
+
     for compare1 in "$array[[@]]";do
+
+        line_count1=0
+
         for compare2 in "$array2[[@]]";do
-            if [[ $compare1[] -eq $compare2[] ]];then
-                
-                echo "Alert detected!"
+
+            line_count2=0
+            line_count1=line_count1 + 1
+            line_count2=line_count2 + 1 
+
+            if [[ $array1[compare1] -eq $array2[compare2] ]];then
+
+                echo "Match detected!"
+                echo "Signature rule at line $line_count2."
+                echo "Detection artefact at line $line_count1."
 
             else
 
                 echo "No alert was detected in the log file."
+                
             fi
         done
     done
